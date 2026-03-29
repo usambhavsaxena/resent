4c7f556a8396e42c3665de50c745afcd56482728
Sambhav Saxena <sambhavsaxena02@gmail.com>
2026-03-29 15:55:25 +0530

    fixed html body param names to match the schema
    Signed-off-by: Sambhav Saxena <sambhavsaxena02@gmail.com>

--- webhook.js
+++ webhook.js
@@ -16,7 +16,8 @@ const save_to_db = async (event, content) => {
         from_email: event.data.from || "",
         to_email: event.data.to?.[0] || "",
         subject: event.data.subject || "",
-        text: content?.text || "",
+        html_body: content?.html || "",
+        text_body: content?.text || "",
         resend_message_id: event.data.message_id || "",
         received_at: new Date().toISOString(),
     });
