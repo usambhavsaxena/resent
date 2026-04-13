42d548a93a506c5c1cee49d48e6fb14c0bad7e86
Sambhav Saxena <sambhavsaxena02@gmail.com>
2026-04-13 17:38:46 +0530

    enabled dirty cors
    Signed-off-by: Sambhav Saxena <sambhavsaxena02@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
 import webhookRouter from "./webhook.js";
 
 const app = express();
+
+app.use(cors({
+  origin: ["http://localhost:8080"],
+  methods: ["POST"],
+}));
 
 app.use(express.json());
 app.use("/api", webhookRouter);
